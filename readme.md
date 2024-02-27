e9f6fcb3d1c5c33c1ac9aee5ab06c1f9c98fcb2e
jiang-24-gdufs <411690076@qq.com>
2024-02-27 23:28:43 +0800

    Init ethers repo

---
+++ readme.md
@@ -0,0 +1,23 @@
+1. connect to Ethereum with RPC Provider and send transaction
+   - [Wallet](https://docs.ethers.org/v6/api/wallet/#Wallet)
+   > A Wallet manages a single private key which is used to sign 
+   > transactions, messages and other common payloads.
+   
+   > 钱包管理一个私钥，用于签署交易、消息和其他常见有效负载。 
+   >
+   - [Signer](https://docs.ethers.org/v6/api/providers/#Signer)
+   - [TransactionRequest](https://docs.ethers.org/v6/api/providers/#TransactionRequest)
+   - [TransactionResponse](https://docs.ethers.org/v6/api/providers/#TransactionResponse)
+   - [TransactionReceipt](https://docs.ethers.org/v6/api/providers/#TransactionReceipt)
+   - 
+
+2.
+
+
+TODO:
+1. 批量创建钱包
+2. 单一转账
+3. 多对多转账
+4. uniswap testbet swap
+5. bridge
+6. lending
---
+++ scripts/01_.js
@@ -0,0 +1,35 @@
+import { ethers } from 'ethers';
+import { config } from 'dotenv';
+console.log(ethers);
+config();
+async function main() {
+  // Configuring the connection to an Ethereum node
+  const network = process.env.ETHEREUM_NETWORK;
+  const provider = new ethers.InfuraProvider(
+    network,
+    process.env.INFURA_API_KEY
+  );
+  // Creating a signing account from a private key
+  const signer = new ethers.Wallet(process.env.SIGNER_PRIVATE_KEY, provider);
+
+  // Creating and calling the transaction object
+  const callTx = await signer.call({
+    to: "0x40f4B71a599851a4b5f28913F5F7aAD967D7C704",
+    value: ethers.parseUnits("0.001", "ether"),
+  });
+  console.log("Calling transaction...", callTx);
+  // Creating and sending the transaction object
+  const tx = await signer.sendTransaction({
+    to: "0x40f4B71a599851a4b5f28913F5F7aAD967D7C704",
+    value: ethers.parseUnits("0.001", "ether"),
+  });
+  console.log("Mining transaction...");
+  console.log(`https://${network}.etherscan.io/tx/${tx.hash}`);
+  // Waiting for the transaction to be mined
+  const receipt = await tx.wait();
+  // The transaction is now on chain!
+  console.log(`Mined in block ${receipt.blockNumber}`);
+}
+
+main();
+
